52d73a5822530bc5afbc462ffe5a9ccbfc39bd6b
leo <liuqing.cao@duitang.com>
2023-07-31 16:56:52 +0800

    feat: 1.UI调整样式。

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "MarqueeText",
     platforms: [
-        SupportedPlatform.iOS(.v13),
-        SupportedPlatform.macOS(.v10_15),
+        SupportedPlatform.iOS(.v14),
+        SupportedPlatform.macOS(.v11),
         SupportedPlatform.watchOS(.v7),
-        SupportedPlatform.tvOS(.v13)
+        SupportedPlatform.tvOS(.v14)
 
     ],
     products: [
